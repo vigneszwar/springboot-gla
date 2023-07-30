e63b606ee31eb1e33551506fcc616ac52a484f6d
Anmolpandey27 <anmolpandey0275@gmail.com>
2023-07-30 21:29:13 +0530

    flight

--- src/main/java/com/demo/springbootservice/FlightController.java
+++ src/main/java/com/demo/springbootservice/FlightController.java
@@ -1,38 +1,44 @@
 package com.demo.springbootservice;
 
 import com.demo.springbootservice.model.Flight;
+import com.demo.springbootservice.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/flights")
 public class FlightController {
     @Autowired
-    ArrayList<Flight> flights;
-    @GetMapping("/flights")
+    FlightRepository flightRepository;
+    @GetMapping
     public List<Flight> getFlights() {
-        return flights;
+        return flightRepository.findAll();
     }
 
-    @PostMapping("/flights")
+    @PostMapping
     public Flight addFlight(@RequestBody Flight newFlight) {
-        flights.add(newFlight);
-        return newFlight;
-
+        Flight flight = flightRepository.save(newFlight);
+        return flight;
     }
 
-    @DeleteMapping("/flights/{name}")
-    public void deleteFlight(@PathVariable String name) {
-        Flight flight = flights.stream().filter(f -> f.getName().equals(name)).findAny().get();
-        flights.remove(flight);
+    @PutMapping("/{name}")
+    public Flight replaceFlight(@PathVariable String name, @RequestBody Flight newFlight) {
+        return null;
     }
-    //Put Method to update the information
-    @PutMapping("/flights/{name}")
-    public void putFlight(@RequestBody Flight newFlight,@PathVariable String name) {
-        Flight flight = flights.stream().filter(f -> f.getName().equals(name)).findAny().get();
-        flights.set(flights.indexOf(flight),newFlight);
+    @DeleteMapping("/{name}")
+    public void addFlight(@PathVariable String name) {
+
     }
 
 
