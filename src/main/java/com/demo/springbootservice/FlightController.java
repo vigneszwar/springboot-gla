7e4fc796851bc30440913c01cf2b7dd1a0412174
anikateagrawal <anikateag7316@gmail.com>
2023-07-30 21:22:27 +0530

    added put mapping

--- src/main/java/com/demo/springbootservice/FlightController.java
+++ src/main/java/com/demo/springbootservice/FlightController.java
@@ -1,38 +1,44 @@
 package com.demo.springbootservice;
 
 import com.demo.springbootservice.model.Flight;
+import com.demo.springbootservice.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/flights")
 public class FlightController {
+
     @Autowired
-    ArrayList<Flight> flights;
-    @GetMapping("/flights")
+    FlightRepository flightRepository;
+    @GetMapping
     public List<Flight> getFlights() {
-        return flights;
+        return flightRepository.findAll();
     }
 
-    @PostMapping("/flights")
+    @GetMapping("/{id}")
+    public Optional<Flight> getFlight(@PathVariable Long id){
+        return flightRepository.findById(id);
+    }
+
+    @PostMapping("")
     public Flight addFlight(@RequestBody Flight newFlight) {
-        flights.add(newFlight);
-        return newFlight;
+        Flight save=flightRepository.save(newFlight);
+        return save;
 
     }
 
-    @DeleteMapping("/flights/{name}")
+    @DeleteMapping("/{name}")
     public void addFlight(@PathVariable String name) {
-        Flight flight = flights.stream().filter(f -> f.getName().equals(name)).findAny().get();
-        flights.remove(flight);
+        return;
     }
 
-    @PutMapping("/flights/{name}")
+    @PutMapping("/{name}")
     public void putFlight(@PathVariable String name,@RequestBody Flight newFlight) {
-        Flight flight = flights.stream().filter(f -> f.getName().equals(name)).findAny().get();
-        flights.set(flights.indexOf(flight),newFlight);
+
     }
 
 
