af122879b5f7049a0d5652d123d118fff6cc5550
vigneszwar <vignezwar@gmail.com>
2023-07-30 11:31:01 +0530

    RESTful API methods

--- src/main/java/com/demo/springbootservice/WelcomeController.java
+++ src/main/java/com/demo/springbootservice/WelcomeController.java
@@ -2,7 +2,11 @@ package com.demo.springbootservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -27,5 +31,25 @@ public class WelcomeController {
     @GetMapping("/name")
     public String getName() {
         return name;
+    }
+
+    @PostMapping
+    public String postMapping(){
+        return "this is post mapping";
+    }
+
+    @PutMapping
+    public String putApi() {
+        return "this is put method api";
+    }
+
+    @PatchMapping
+    public String patchApi() {
+        return "this is patch method api";
+    }
+
+    @DeleteMapping
+    public String deleteApi() {
+        return "this is delete method api";
     }
 }
