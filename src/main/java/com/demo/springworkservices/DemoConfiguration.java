6a7b2f16db3a8c22f55e43dcdcd06daa8a87f520
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-07-30 15:42:00 +0530

    new branch commits

--- src/main/java/com/demo/springworkservices/DemoConfiguration.java
+++ src/main/java/com/demo/springworkservices/DemoConfiguration.java
@@ -1,5 +1,6 @@
 package com.demo.springworkservices;
 
+import com.demo.springworkservices.model.Flight;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,18 @@ import java.util.ArrayList;
 @Configuration
 public class DemoConfiguration {
 
+    @Bean
+    public ArrayList<Flight> getFlights() {
+        ArrayList<Flight> flights = new ArrayList<>();
+        flights.add(Flight.builder()
+                .departure("12:00 pm")
+                .arrival("6:00 pm")
+                .src("Delhi Airport")
+                .dest("Bangalore Airport")
+                .name("Indigo")
+                .build());
+        return flights;
+    }
     @Bean
     public ArrayList<Integer> getNumbers() {
         System.out.println("Inside Bean method which creates and returns arraylist to spring boot");
---
+++ src/main/java/com/demo/springworkservices/FlightController.java
@@ -0,0 +1,40 @@
+package com.demo.springworkservices;
+
+
+import com.demo.springworkservices.model.Flight;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class FlightController {
+    @Autowired
+    ArrayList<Flight> flights;
+    @GetMapping("/flights")
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    @PostMapping("/flights")
+    public Flight addFlight(@RequestBody Flight newFlight) {
+        flights.add(newFlight);
+        return newFlight;
+
+    }
+
+    @DeleteMapping("/flights/{name}")
+    public void addFlight(@PathVariable String name) {
+        Flight flight = flights.stream().filter(f -> f.getName().equals(name)).findAny().get();
+        flights.remove(flight);
+    }
+
+    @PutMapping("/flights/{name}")
+    public void putFlight(@PathVariable String name,@RequestBody Flight newFlight) {
+        Flight flight = flights.stream().filter(f -> f.getName().equals(name)).findAny().get();
+        flights.set(flights.indexOf(flight),newFlight);
+    }
+
+
+}
