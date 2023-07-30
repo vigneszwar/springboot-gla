b3724a564d4ac8c04dcb597421898257ea289087
Manvi1108 <manvisinghal285@gmail.com>
2023-07-30 23:04:22 +0530

    mysql & ORM Changes

--- src/main/java/com/demo/springbootservice/FlightController.java
+++ src/main/java/com/demo/springbootservice/FlightController.java
@@ -1,44 +1,58 @@
 package com.demo.springbootservice;
 
 import com.demo.springbootservice.model.Flight;
+import com.demo.springbootservice.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/flights")
 public class FlightController {
     @Autowired
-    ArrayList<Flight> flights;
-    @GetMapping("/flights")
-    public List<Flight> getFlights() {
-        return flights;
+    FlightRepository flightRepository;
+    @GetMapping
+    public List<Flight> getFlights(@RequestParam(required = false) String name, @RequestParam(required = false) String src) {
+        if(name != null && !name.isEmpty()) {
+            Flight flight = flightRepository.findByName(name);
+            return List.of(flight);
+        }
+        if(src != null && !src.isEmpty()) {
+            return flightRepository.findAllBySrc(src);
+        }
+        return flightRepository.findAll();
     }
 
-    @PostMapping("/flights")
+    @GetMapping("{id}")
+    public Flight getFlight(@PathVariable Long id) {
+        return flightRepository.findById(id).get();
+    }
+
+    @PostMapping
     public Flight addFlight(@RequestBody Flight newFlight) {
-        flights.add(newFlight);
-        return newFlight;
-
+        Flight flight = flightRepository.save(newFlight);
+        return flight;
     }
 
-    @DeleteMapping("/flights/{name}")
-    public void addFlight(@PathVariable String name) {
-        Flight flight = flights.stream().filter(f -> f.getName().equals(name)).findAny().get();
-        flights.remove(flight);
+    @PutMapping("/{id}")
+    public Flight replaceFlight(@PathVariable long id, @RequestBody Flight newFlight) {
+        return flightRepository.save(newFlight);
+    }
+    @DeleteMapping("/{id}")
+    public void addFlight(@PathVariable long id) {
+        flightRepository.deleteById(id);
     }
 
-    @PutMapping("/flights/{name}")
-    public void putFlight(@PathVariable String name,@RequestBody Flight newFlight) {
-        Flight flight = flights.stream().filter(f -> f.getName().equals(name)).findAny().get();
-        flights.set(flights.indexOf(flight),newFlight);
-    }
 
 }
---
+++ src/main/java/com/demo/springbootservice/repository/FlightRepository.java
@@ -0,0 +1,13 @@
+package com.demo.springbootservice.repository;
+
+import com.demo.springbootservice.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+    Flight findByName(String name);
+    List<Flight> findAllBySrc(String src);
+}
