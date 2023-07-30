a33b32ccbb841191df9e357a95813e8d2f8f7108
CodeBurner <codeburner0@gmail.com>
2023-07-30 14:45:56 +0530

    my put changes commit

--- src/main/java/com/demo/springbootservice/FlightController.java
+++ src/main/java/com/demo/springbootservice/FlightController.java
@@ -2,12 +2,7 @@ package com.demo.springbootservice;
 
 import com.demo.springbootservice.model.Flight;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +21,16 @@ public class FlightController {
         flights.add(newFlight);
         return newFlight;
 
+    }
+    @PutMapping("/flights/{name}")
+    public List<Flight> updateFlight(@PathVariable String name,@RequestBody Flight flightDetails) {
+        Flight updateFlight = flights.stream().filter(f -> f.getName().equals(name)).findAny().get();
+        updateFlight.setSrc(flightDetails.getSrc());
+        updateFlight.setDest(flightDetails.getDest());
+        updateFlight.setDeparture(flightDetails.getDeparture());
+        updateFlight.setArrival(flightDetails.getArrival());
+        flights.add(updateFlight);
+        return flights;
     }
 
     @DeleteMapping("/flights/{name}")
