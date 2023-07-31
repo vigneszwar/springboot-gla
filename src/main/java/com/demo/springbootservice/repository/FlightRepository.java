09581621f28f2124216a9841d89063ed7eaa9262
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-07-31 11:52:50 +0530

    commit

--- src/main/java/com/demo/springbootservice/repository/FlightRepository.java
+++ src/main/java/com/demo/springbootservice/repository/FlightRepository.java
@@ -1,13 +1,8 @@
-package com.demo.springbootservice.repository;
+package com.demo.springbootservice.Repository;
 
-import com.demo.springbootservice.model.Flight;
+import com.demo.springbootservice.Model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
-     Flight findByName(String name);
-     List<Flight> findAllBySrc(String src);
+    Flight findByName(String name);
 }
